c1aa9c1d36fec3c474a60478af1d452b5bde8099
Sarah Naas <sjnaas@gmail.com>
2025-05-18 20:35:12 +0200

    sizing

--- src/app/components/TextInput.tsx
+++ src/app/components/TextInput.tsx
@@ -7,7 +7,7 @@ function TextInput({ onChange }: TextInputProps) {
     <input
       onChange={onChange}
       type="text"
-      className="border bg-[#ffffff] rounded-md"
+      className="border bg-[#ffffff] rounded-sm w-full py-2"
     />
   );
 }
