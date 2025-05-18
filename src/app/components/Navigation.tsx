ac8a815fc6894e3dbabf8e467da40b28e18981b9
Sarah Naas <sjnaas@gmail.com>
2025-05-18 22:48:11 +0200

    Navigation

---
+++ src/app/components/Navigation.tsx
@@ -0,0 +1,9 @@
+function TextInput() {
+  return (
+    <nav className="border-b w-full">
+      <h1 className="text-4xl font-light">MoodMatcher</h1>
+    </nav>
+  );
+}
+
+export default TextInput;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 import Button from "./components/Button";
+import Navigation from "./components/Navigation";
 import TextInput from "./components/TextInput";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-josefin">
-      <nav className="border-b">
-        <h1 className="text-4xl font-bold mb-6">MoodMatcher</h1>
-      </nav>
+      <Navigation />
       <h2>How are you feeling?</h2>
 
       <h2>Let's get a feel for the weather.</h2>
