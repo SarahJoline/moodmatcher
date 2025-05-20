ae94fb6749221524c927cf834d6f287a8d7f51db
Sarah Naas <sjnaas@gmail.com>
2025-05-20 10:00:37 +0200

    basic state and styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,49 @@
+"use client";
+
+import { useState } from "react";
 import Button from "./components/Button";
 import Navigation from "./components/Navigation";
 import TextInput from "./components/TextInput";
 
 export default function Home() {
+  let [userMood, setUserMood] = useState({ mood: "", location: "" });
+  let moods = [
+    "Happy",
+    "Sad",
+    "Angry",
+    "Sleepy",
+    "Excited",
+    "Anxious",
+    "Calm",
+    "Lonely",
+  ];
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-josefin">
+    <div className="grid grid-rows-[40px_20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-5 sm:p-20 font-josefin">
       <Navigation />
-      <h2>How are you feeling?</h2>
+      {userMood.mood === "" && (
+        <>
+          <h2 className="py-1">How are you feeling?</h2>
 
-      <h2>Let's get a feel for the weather.</h2>
-      <TextInput />
-      <Button />
+          <div className="grid grid-cols-2 gap-y-[5rem] gap-x-[7rem] mb-10">
+            {moods.map((mood) => (
+              <button
+                onClick={() => setUserMood({ ...userMood, mood: mood })}
+                key={mood}
+                className="cursor-pointer"
+              >
+                {mood}
+              </button>
+            ))}
+          </div>
+        </>
+      )}
+      {userMood.mood !== "" && (
+        <>
+          <h2 className="py-1">Let's get a feel for the weather.</h2>
+          <TextInput />
+          <Button />
+        </>
+      )}
     </div>
   );
 }
